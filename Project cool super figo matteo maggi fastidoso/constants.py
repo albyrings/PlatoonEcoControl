d3d89e5befc23fc99dadc2f0b25dbba367a9584a
AlbertoAnelli <alberto.anelli01@universitadipavia.it>
2025-06-03 10:28:48 +0200

    Delete Project cool super figo matteo maggi fastidoso directory

--- Project cool super figo matteo maggi fastidoso/constants.py
+++
@@ -1,7 +0,0 @@
-# Constants for energy computation
-b1 = 0.1  # Coefficient for link energy
-b2 = 0.05  # Coefficient for link energy
-h1 = 1.0  # Coefficient for driving input
-h2 = 0.2  # Coefficient for driving input
-h3 = 0.1  # Coefficient for driving input
-h0 = 0.05  # Constant driving input offset
